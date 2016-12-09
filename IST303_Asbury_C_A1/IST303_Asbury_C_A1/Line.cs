fafc6778369b8cc747570c113dcc8280248d7263
Cameron Asbury <asbury56@marshall.edu>
2016-12-09 00:42:57 -0500

    Added Assignment 1

---
+++ IST303_Asbury_C_A1/IST303_Asbury_C_A1/Line.cs
@@ -0,0 +1,160 @@
+﻿//Cameron Asbury
+//C# Assignment 1
+//File 1 of 2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST303_Asbury_C_A1
+{
+    class Line
+    {
+        //the a, b, and c of the standard form of a line ax + by = c
+        //Given the automatic get set properties
+        private double A { get; set; }
+        private double B { get; set; }
+        private double C { get; set; }
+       
+        //private line class variables
+        private double slope;
+        private bool isVertical;
+        private bool isHorizontal;
+
+        //Default constructor
+        public Line()
+        {
+
+        }
+
+        //Constructor with the three variables in it
+        public Line(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        //Method to determine if the two lines are perpindicular, parallel, or neither
+        public string perpOrPar(Line line1, Line line2)
+        {
+            //Strings to return if one of the 3.
+            string pd = "perpindicular.";
+            string pr = "parallel.";
+            string n = "neither perpindicular or parallel.";
+
+            //Compares the lines to eachother to determine if one of the 3 options.
+             if ((line1.isHorizontal == true && line2.isVertical == true)|| (line2.isHorizontal == true && line1.isVertical == true))
+            {
+                return pd;
+            }
+
+             if (line1.isVertical == true && line2.isVertical == true)
+            {
+                return pr;
+            }
+
+             else 
+                return n;
+        }
+
+        //Method to find Y if given X.
+        public double LineFindY(double x)
+        {
+            double y = 0;
+            y = ((C - (A * x)) / B);
+            return y;
+        }
+        //Method to find X if given Y.
+        public double LineFindX(double y)
+        {
+            double x = 0;
+            x = ((C - (B * y)) / A);
+            return x;
+        }
+
+        //Method to determine if the line is vertical or horizontal
+        public void isVertOrHorz()
+        {
+            //Set of if/else to make sure lines are given correct Horizontal or Vertical tags.
+            if (B == 0)
+            {
+                isVertical = true;
+            }
+            if (A == 0)
+            {
+                isHorizontal = true;
+                setSlope();
+            }
+            else
+            {
+                setSlope();
+            }
+
+        }
+
+        //Method to set the slope variable
+        //Made private as to not give user power to create false values.
+        private void setSlope()
+        {
+            slope = (-1 * A) / B;
+        }
+        //Public method to allow user to return the value of the slope.
+        public double getSlope()
+        {
+            return slope;
+        }
+        //Method that determines where the two lines intersect if they do intersect.
+        //Could not get algorithm worked out myself, used another's help to determine the X value
+        //https://www.topcoder.com/community/data-science/data-science-tutorials/geometry-concepts-line-intersection-and-its-applications/
+        public void whereIntersect(Line line1, Line line2)
+        {
+            //local variables
+            double x, y = 0;
+            double a1, b1, c1, a2, b2, c2;
+            
+            //Contructor Variables
+            a1 = line1.A;
+            b1 = line1.B;
+            c1 = line1.C;
+            a2 = line2.A;
+            b2 = line2.B;
+            c2 = line2.C;
+
+            //Algorithm to determine X value of line intersection.
+            x = ((b2 * c1) - (b1 * c2)) / ((a1 * b2) - (a2 * b1));
+            //Determines Y value of intersection once X value is found.
+            y = line1.LineFindY(x);
+
+            //Prints to the screen if lines intersect or not.
+            if ((line1.isVertical && line2.isVertical) || (line1.isHorizontal && line2.isHorizontal) != true)
+                Console.WriteLine("The two lines intersect at ({0},{1}).", x, y);
+
+            else
+                Console.WriteLine("Lines are parallel");
+        }
+
+                
+    }
+
+}
+        
+
+
+       
+
+            
+
+
+       
+
+
+      
+
+                
+
+    
+
+
+   
---
+++ IST303_Asbury_C_A1/IST303_Asbury_C_A1/Program.cs
@@ -0,0 +1,49 @@
+﻿//Cameron Asbury
+//C# Assignment 1
+//File 1 of 2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST303_Asbury_C_A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //local variables
+            double xValue1 = 5;
+            double yValue1 = 3;
+
+            double xValue2 = 4;
+            double yValue2 = 7;
+
+            //Our two lines to compare to eachother.
+            Line line1 = new Line(4,5,6);
+            Line line2 = new Line(2,4,7);
+
+            //Extra Line type in order to compare in the background.
+            Line compare = new Line();
+
+
+            //Methods being used and/or printed to screen.
+            Console.WriteLine("The X value for line 1 when Y = {0} is {1}", yValue1, line1.LineFindX(xValue1));
+            Console.WriteLine("The X value for line 2 when Y = {0} is {1}", yValue2, line2.LineFindX(xValue2));
+
+            Console.WriteLine("The Y value for line 1 when X = {0} is {1}", xValue1, line1.LineFindY(yValue1));
+            Console.WriteLine("The Y value for line 2 when X = {0} is {1}", xValue2, line2.LineFindY(yValue2));
+
+            line1.isVertOrHorz();
+            line2.isVertOrHorz();
+            Console.WriteLine("The slope for line 1 is {0}",line1.getSlope());
+            Console.WriteLine("The slope for line 2 is {0}",line2.getSlope());
+
+            Console.WriteLine("Line 1 and Line 2 are {0}", compare.perpOrPar(line1, line2));
+
+            compare.whereIntersect(line1,line2);
+
+        }
+    }
+}
