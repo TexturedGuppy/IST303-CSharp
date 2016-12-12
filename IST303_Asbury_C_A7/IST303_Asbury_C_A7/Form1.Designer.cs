994feab33a00517a1caa40fd9ae84de81a0be052
Cameron Asbury <asbury56@marshall.edu>
2016-12-12 15:47:13 -0500

    Basically Done, Minor bugs

---
+++ IST303_Asbury_C_A7/IST303_Asbury_C_A7/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace IST303_Asbury_C_A7
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSelection = new System.Windows.Forms.Label();
+            this.rchTxtFilesAndFolders = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(156, 562);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(116, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Current Folder Only";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 556);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 487);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // lblSelection
+            // 
+            this.lblSelection.AutoSize = true;
+            this.lblSelection.Location = new System.Drawing.Point(12, 487);
+            this.lblSelection.Name = "lblSelection";
+            this.lblSelection.Size = new System.Drawing.Size(86, 13);
+            this.lblSelection.TabIndex = 3;
+            this.lblSelection.Text = "Folder Selection:";
+            // 
+            // rchTxtFilesAndFolders
+            // 
+            this.rchTxtFilesAndFolders.Location = new System.Drawing.Point(15, 12);
+            this.rchTxtFilesAndFolders.Name = "rchTxtFilesAndFolders";
+            this.rchTxtFilesAndFolders.Size = new System.Drawing.Size(719, 472);
+            this.rchTxtFilesAndFolders.TabIndex = 4;
+            this.rchTxtFilesAndFolders.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 591);
+            this.Controls.Add(this.rchTxtFilesAndFolders);
+            this.Controls.Add(this.lblSelection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "frmMain";
+            this.Text = "FolderBrowser";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSelection;
+        private System.Windows.Forms.RichTextBox rchTxtFilesAndFolders;
+    }
+}
+
---
+++ IST303_Asbury_C_A7/IST303_Asbury_C_A7/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace IST303_Asbury_C_A7
+{
+    public partial class frmMain : Form
+    {
+        FolderBrowserDialog fileBrowser = new FolderBrowserDialog();
+        string fileFolder;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            fileBrowser.ShowDialog();
+            fileFolder = fileBrowser.SelectedPath;
+            label1.Text = fileFolder;
+            label1.Visible = true;
+
+            if (fileBrowser.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+            {
+
+            }
+
+            else
+            {
+                if (checkBox1.Checked)
+                {
+                    string[] files = Directory.GetFileSystemEntries(fileFolder, "*", SearchOption.TopDirectoryOnly);
+                    foreach (string line in files)
+                        rchTxtFilesAndFolders.AppendText(line + "\n");
+                }
+                else
+                {
+                    string[] files = Directory.GetFileSystemEntries(fileFolder, "*", SearchOption.AllDirectories);
+                    foreach (string line in files)
+                        rchTxtFilesAndFolders.AppendText(line + "\n");
+
+                }
+
+            }
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+    }
+}
