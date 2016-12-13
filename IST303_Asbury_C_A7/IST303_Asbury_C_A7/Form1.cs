15da56a244cb278a7bae8751990de6a19c43ff26
Cameron Asbury <asbury56@marshall.edu>
2016-12-13 00:39:10 -0500

    Assignment 7 done and commented

--- IST303_Asbury_C_A7/IST303_Asbury_C_A7/Form1.cs
+++ IST303_Asbury_C_A7/IST303_Asbury_C_A7/Form1.cs
@@ -14,8 +14,11 @@ namespace IST303_Asbury_C_A7
 {
     public partial class frmMain : Form
     {
-        FolderBrowserDialog fileBrowser = new FolderBrowserDialog();
-        string fileFolder;
+        
+       
+       //Program Variables
+        FolderBrowserDialog fileBrowser = new FolderBrowserDialog(); //The File Browser 
+        string fileFolder; //String to store file path
 
         public frmMain()
         {
@@ -23,39 +26,67 @@ namespace IST303_Asbury_C_A7
         }
 
        
-
+        /// <summary>
+        /// Controls Basically Entire Program
+        /// Checks to see if you want to see Top Directory or All Directories with the CheckBox
+        /// Controls the Adding of Threads when calling the Print Function
+        /// Asynchronous
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void btnSearch_Click(object sender, EventArgs e)
         {
-            fileBrowser.ShowDialog();
+            
+            DialogResult d = fileBrowser.ShowDialog();
             fileFolder = fileBrowser.SelectedPath;
             label1.Text = fileFolder;
             label1.Visible = true;
 
-            if (fileBrowser.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
-            {
-
-            }
-
-            else
+            if (d == DialogResult.OK)
             {
                 if (checkBox1.Checked)
                 {
+                    //string[] files = Directory.GetFileSystemEntries(fileFolder, "*", SearchOption.TopDirectoryOnly);
+                    //foreach (string line in files)
+                    //    rchTxtFilesAndFolders.AppendText(line + "\n");
                     string[] files = Directory.GetFileSystemEntries(fileFolder, "*", SearchOption.TopDirectoryOnly);
-                    foreach (string line in files)
-                        rchTxtFilesAndFolders.AppendText(line + "\n");
+                    await Task.Run(() => GetAndPrint(files));
                 }
                 else
                 {
+                    //string[] files = Directory.GetFileSystemEntries(fileFolder, "*", SearchOption.AllDirectories);
+                    //foreach (string line in files)
+                    //    rchTxtFilesAndFolders.AppendText(line + "\n");
                     string[] files = Directory.GetFileSystemEntries(fileFolder, "*", SearchOption.AllDirectories);
-                    foreach (string line in files)
-                        rchTxtFilesAndFolders.AppendText(line + "\n");
+
+                    await Task.Run(() => GetAndPrint(files));
 
                 }
-
             }
+            else
+            {
+                label1.Text = "No Folder or File Selected";
+            }
+
 
         }
 
+        /// <summary>
+        /// Takes in a string array and prints it out one by one into the Rich Text Box
+        /// </summary>
+        /// <param name="files"></param>
+        private void GetAndPrint(string [] files)
+        {
+            foreach (string line in files)
+                rchTxtFilesAndFolders.AppendText(line + "\n");
+        }
+
+        /// <summary>
+        /// Form Loading Function
+        /// Currently just controls the label for letting you know what file path you're on
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmMain_Load(object sender, EventArgs e)
         {
             label1.Visible = false;
