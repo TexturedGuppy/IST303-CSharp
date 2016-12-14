97360dd0b5a96db235ce8c939a5e129229bcd75e
Cameron Asbury <asbury56@marshall.edu>
2016-12-13 23:49:30 -0500

    Finished as much as I could
    Still missing all the story functionality

--- IST303_Asbury_C_A6/IST303_Asbury_C_A6/Form1.Designer.cs
+++ IST303_Asbury_C_A6/IST303_Asbury_C_A6/Form1.Designer.cs
@@ -37,15 +37,18 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.txtToDo = new System.Windows.Forms.TextBox();
             this.lblTest = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lstList
             // 
             this.lstList.FormattingEnabled = true;
-            this.lstList.Location = new System.Drawing.Point(425, 34);
+            this.lstList.Location = new System.Drawing.Point(215, 34);
             this.lstList.Name = "lstList";
-            this.lstList.Size = new System.Drawing.Size(156, 264);
+            this.lstList.Size = new System.Drawing.Size(366, 264);
             this.lstList.TabIndex = 0;
+            this.lstList.TabStop = false;
             // 
             // btnQuit
             // 
@@ -54,6 +57,7 @@
             this.btnQuit.Name = "btnQuit";
             this.btnQuit.Size = new System.Drawing.Size(75, 23);
             this.btnQuit.TabIndex = 1;
+            this.btnQuit.TabStop = false;
             this.btnQuit.Text = "&Quit";
             this.btnQuit.UseVisualStyleBackColor = true;
             this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
@@ -63,62 +67,85 @@
             this.btnAdd.Location = new System.Drawing.Point(12, 421);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
-            this.btnAdd.TabIndex = 2;
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.TabStop = false;
             this.btnAdd.Text = "&Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(93, 421);
+            this.btnRemove.Location = new System.Drawing.Point(204, 418);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(75, 23);
-            this.btnRemove.TabIndex = 3;
+            this.btnRemove.TabIndex = 2;
             this.btnRemove.Text = "&Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // btnSortByName
             // 
-            this.btnSortByName.Location = new System.Drawing.Point(174, 421);
+            this.btnSortByName.Location = new System.Drawing.Point(285, 418);
             this.btnSortByName.Name = "btnSortByName";
             this.btnSortByName.Size = new System.Drawing.Size(81, 23);
-            this.btnSortByName.TabIndex = 4;
+            this.btnSortByName.TabIndex = 3;
             this.btnSortByName.Text = "Sort By &Name";
             this.btnSortByName.UseVisualStyleBackColor = true;
+            this.btnSortByName.Click += new System.EventHandler(this.btnSortByName_Click);
             // 
             // btnSortByDate
             // 
-            this.btnSortByDate.Location = new System.Drawing.Point(261, 421);
+            this.btnSortByDate.Location = new System.Drawing.Point(372, 418);
             this.btnSortByDate.Name = "btnSortByDate";
             this.btnSortByDate.Size = new System.Drawing.Size(81, 23);
-            this.btnSortByDate.TabIndex = 5;
+            this.btnSortByDate.TabIndex = 4;
             this.btnSortByDate.Text = "Sort By &Date";
             this.btnSortByDate.UseVisualStyleBackColor = true;
+            this.btnSortByDate.Click += new System.EventHandler(this.btnSortByDate_Click);
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(12, 221);
+            this.dateTimePicker1.MinDate = new System.DateTime(2016, 12, 13, 0, 0, 0, 0);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(197, 20);
             this.dateTimePicker1.TabIndex = 6;
             this.dateTimePicker1.Value = new System.DateTime(2016, 12, 13, 16, 14, 54, 0);
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // txtToDo
             // 
-            this.txtToDo.Location = new System.Drawing.Point(348, 423);
+            this.txtToDo.Location = new System.Drawing.Point(98, 421);
             this.txtToDo.Name = "txtToDo";
             this.txtToDo.Size = new System.Drawing.Size(100, 20);
-            this.txtToDo.TabIndex = 7;
+            this.txtToDo.TabIndex = 1;
             // 
             // lblTest
             // 
             this.lblTest.AutoSize = true;
-            this.lblTest.Location = new System.Drawing.Point(141, 302);
+            this.lblTest.ForeColor = System.Drawing.Color.Red;
+            this.lblTest.Location = new System.Drawing.Point(381, 344);
             this.lblTest.Name = "lblTest";
             this.lblTest.Size = new System.Drawing.Size(35, 13);
             this.lblTest.TabIndex = 8;
             this.lblTest.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Manually Type in Numbers in the Time or use arrow keys";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Calendar Works by Clicking the Calendar Icon";
             // 
             // frmMain
             // 
@@ -127,6 +154,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnQuit;
             this.ClientSize = new System.Drawing.Size(593, 456);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblTest);
             this.Controls.Add(this.txtToDo);
             this.Controls.Add(this.dateTimePicker1);
@@ -138,6 +167,7 @@
             this.Controls.Add(this.lstList);
             this.Name = "frmMain";
             this.Text = "To Do List";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -155,6 +185,8 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.TextBox txtToDo;
         private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- IST303_Asbury_C_A6/IST303_Asbury_C_A6/Form1.cs
+++ IST303_Asbury_C_A6/IST303_Asbury_C_A6/Form1.cs
@@ -13,45 +13,148 @@ namespace IST303_Asbury_C_A6
 {
     public partial class frmMain : Form
     {
-        DateTimePicker date = new DateTimePicker();
-        private DateTimePicker timePicker;
-        List<string> toDoList = new List<string>();
+        DateTimePicker date = new DateTimePicker(); //My DateTimePicker Control Variable
+        List<string> LtoDoList = new List<string>();//to do list of type list
+        List<DateTime> Ldate = new List<DateTime>();//Was going to use for sorting the dates
+        List<string> Lnames = new List<string>();//Was going to use for sorting names
+        string file = "toDoList.txt"; //Text File to save and read from
+        string name; //Name of the task on the todo list
 
         public frmMain()
         {
+
             InitializeComponent();
 
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
+       
+        /// <summary>
+        /// Add button functionality
+        /// Checks to see if name text is empty before allowing user to input a task.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            lblTest.Text = "Date:" + date.Value.ToShortDateString() + " Time:" + date.Value.ToShortTimeString();
-            date.Value = DateTime.Now;
-            toDoList.Add("Date:" + date.Value.ToShortDateString() + " Time:" + date.Value.ToShortTimeString());
-            lstList.DataSource = toDoList;
-            
+            if (!string.IsNullOrEmpty(txtToDo.Text))
+            {
+                name = txtToDo.Text;
+                lblTest.Visible = false;
+                string dateAndTime = "Date:" + date.Value.ToShortDateString() + " Time:" + date.Value.ToShortTimeString() + " Task:" + name;
+                name = txtToDo.Text;
+                lblTest.Text = dateAndTime.Split(':', ' ')[1];
+                Ldate.Add(date.Value);
+                LtoDoList.Add(dateAndTime);
+                lstList.Items.Add(dateAndTime);
+                date.Value = DateTime.Now;
+                txtToDo.Clear();
+            }
+            else
+            {
+                lblTest.Visible = true;
+                lblTest.Text = "No Name for your To Do List Item.";
+            }
+
         }
 
+        /// <summary>
+        /// What happens when the form is loaded.
+        /// This is where the magic happens if you already have text saved to a file
+        /// Reads from the file and stores the information to listBox and to the List
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmMain_Load(object sender, EventArgs e)
         {
-            
-            //date.Format = DateTimePickerFormat.Custom;
+
+            lblTest.Visible = false;
             date = dateTimePicker1;
+            LtoDoList = File.ReadLines(file).ToList();
+            date.Value = DateTime.Now;
+            string[] _file = File.ReadAllLines(file);
+            lstList.Items.AddRange(_file);
+            //date.Format = DateTimePickerFormat.Custom;
             date.CustomFormat = "MM/dd/yyyy     hh:mm:ss tt";
             date.Format = DateTimePickerFormat.Custom;
+            date.MinDate = DateTime.Today;
+            //foreach(string str in LtoDoList)
+            //{
+            //    Ldate.Add(Convert.ToDateTime(str));
+            //}
         }
 
+        /// <summary>
+        /// QUit button
+        /// does exactly what you think it would
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnQuit_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-       
-        
 
+        /// <summary>
+        /// Have the form write to the file upon closing as to not lose any data between sessions
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllLines(file, LtoDoList);
+        }
+
+        /// <summary>
+        /// Remove button
+        /// Will remove any selected item from the listbox
+        /// After removing it will update the normal list as well
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            lstList.Items.Remove(lstList.SelectedItem);
+            LtoDoList.Clear();
+            foreach (string str in lstList.Items)
+            {
+                LtoDoList.Add(str);
+            }
+            
+        }
+
+        /// <summary>
+        /// Sort by date function, that is incomplete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSortByDate_Click(object sender, EventArgs e)
+        {
+            
+                //var dateSorted = from str in LtoDoList orderby date ascending select Convert.ToDateTime(str.Substring(str.IndexOf(":") + 1,str.IndexOf(" ") - 1));
+                var dateSorted = from str in LtoDoList orderby date ascending select str.Split(':', ' ')[1];
+                lstList.Items.Clear();
+
+                foreach (string str in dateSorted)
+                {
+                    lstList.Items.Add(str);
+                }
+
+                LtoDoList.Clear();
+            foreach (string str in lstList.Items)
+            {
+                LtoDoList.Add(str);
+            }
+
+
+
+        }
+
+        private void btnSortByName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
 }
